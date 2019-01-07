b013a9904452986f007d1212f14b1c443745346c
Liupinz <2663480697@qq.com>
2019-01-07 20:24:43 +0800

    add uemail charfield to OM_user database

--- openstackM/OM/models.py
+++ openstackM/OM/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class User(models.Model):
     uname = models.CharField(max_length=30)
     upassword = models.CharField(max_length=10)
+    uemail = models.EmailField(null=True, blank=True)
